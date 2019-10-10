dd96c22b9a2e52cd2bcbde90d00f6c8a441f5d2a
Sean <sthorpe1@asu.edu>
2019-10-09 21:40:15 -0700

    Initial Commit

---
+++ CSE360-Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assign2;
+/**
+ * File includes constructor for AddingMachine 
+ * total as well as the getTotal(), add(), subtract(), toString(), 
+ * and clear() functions.
+ * @author Sean F. Thorpe
+ * @param total This is the private int used to track total number value.
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * AddingMachine() is the constructor for the AddingMachine class.
+	 * @param total sets the total to 0 in the constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal() returns the total int variable.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add() takes an integer value and adds it to the total variable.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract() takes an integer value and 
+	 * subtracts it from the total variable.
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 *	toString() converts the value into a string. 
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * clear() clears out the value.	 
+	 */
+	public void clear() {
+	
+	}
+}
